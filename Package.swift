029b6b3f16a1ec8b74415226e26a61340fcce128
Kasper Lahti <kasper@lahti.email>
2021-09-30 14:36:28 +0200

    Add package

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "PageControl",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(name: "PageControl", targets: ["PageControl"])
+    ],
+    targets: [
+        .target(name: "PageControl", exclude: ["Info.plist"]),
+        .testTarget(name: "PageControlTests", dependencies: ["PageControl"], exclude: ["Info.plist"])
+    ]
+)
